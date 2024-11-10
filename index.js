35bac27b823e29b126cc6892ef14eba0e6b7233e
congquan92 <145172432+congquan92@users.noreply.github.com>
2024-11-10 16:34:02 +0700

    4:33 10/11

---
+++ index.js
@@ -0,0 +1,60 @@
+var input = document.querySelector('input');
+var button = document.querySelector('button');
+var form = document.querySelector('form');
+var todoList = document.querySelector('.todo');
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    let val = input.value.trim(); // remove whitespace    
+    if (val) {
+        add({
+            text: val,
+            status: 'incomplete' // default status
+        });
+    }
+    saveTodolist();
+});
+
+function add(todo) {
+    var li = document.createElement('li');
+    li.innerHTML = `
+        <span>${todo.text}</span><i class="fa-solid fa-trash"></i>
+    `;
+    if (todo.status === 'completed') {
+        li.classList.add('complete');
+    }
+    li.addEventListener('click', function () {
+        this.classList.toggle('complete');
+        saveTodolist();
+    });
+    todoList.appendChild(li);
+    li.querySelector('i').addEventListener('click', function () {
+        this.parentElement.remove();
+        saveTodolist();
+    });
+}
+
+function saveTodolist() {
+    let todolistdom = document.querySelectorAll('li');
+    let todolistStore = [];
+    todolistdom.forEach(item => {
+        let text = item.querySelector('span').innerHTML;
+        let status = item.classList.contains('complete') ? 'completed' : 'incomplete';
+        todolistStore.push({
+            text,
+            status
+        });
+    });
+    localStorage.setItem('todolistdom', JSON.stringify(todolistStore));
+}
+
+function int() {
+    let data = JSON.parse(localStorage.getItem('todolistdom'));
+    if (data) {
+        data.forEach(i => {
+            add(i);
+        });
+    }
+}
+
+int();
